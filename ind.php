ba863b9a82a8dee104460c5c13a7bb5ebce51eb1
Lndqw12 <COMP222012016@mail.ru>
2016-10-01 19:33:15 +0300

    mysql

---
+++ ind.php
@@ -0,0 +1,12 @@
+<?php
+$host="localhost";
+$user="root";
+$password="";
+$db="3pk";
+
+mysql_connect($host, $user, $password) or die("MySQL сервер недоступен!".mysql_error());
+mysql_select_db($db) or die("Нет соединения с БД".mysql_error());
+
+$result = mysql_query("SELECT * FROM 3pk");
+echo $result;
+?>
